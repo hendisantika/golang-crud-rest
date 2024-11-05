7b1a0b2e4fb4591000f124ea30a5116c47dfd807
hendisantika <hendisantika@yahoo.co.id>
2024-11-05 17:05:20 +0700

    Add Prepare in db.go🫸🌀✏️📗🐧🐳

--- db/sqlc/db.go
+++ db/sqlc/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type DBTX interface {
@@ -14,4 +15,25 @@ type DBTX interface {
 
 func New(db DBTX) *Queries {
 	return &Queries{db: db}
+}
+
+func Prepare(ctx context.Context, db DBTX) (*Queries, error) {
+	q := Queries{db: db}
+	var err error
+	if q.createContactStmt, err = db.PrepareContext(ctx, createContact); err != nil {
+		return nil, fmt.Errorf("error preparing query CreateContact: %w", err)
+	}
+	if q.deleteContactStmt, err = db.PrepareContext(ctx, deleteContact); err != nil {
+		return nil, fmt.Errorf("error preparing query DeleteContact: %w", err)
+	}
+	if q.getContactByIdStmt, err = db.PrepareContext(ctx, getContactById); err != nil {
+		return nil, fmt.Errorf("error preparing query GetContactById: %w", err)
+	}
+	if q.listContactsStmt, err = db.PrepareContext(ctx, listContacts); err != nil {
+		return nil, fmt.Errorf("error preparing query ListContacts: %w", err)
+	}
+	if q.updateContactStmt, err = db.PrepareContext(ctx, updateContact); err != nil {
+		return nil, fmt.Errorf("error preparing query UpdateContact: %w", err)
+	}
+	return &q, nil
 }
