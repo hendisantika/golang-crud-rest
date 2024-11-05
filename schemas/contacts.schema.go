61522e475a319bef4576fc1380a7f6f72e6cc3f7
hendisantika <hendisantika@yahoo.co.id>
2024-11-05 17:00:50 +0700

    Add CreateContact🫸🌀✏️📗🐧🐳

---
+++ schemas/contacts.schema.go
@@ -0,0 +1,8 @@
+package schemas
+
+type CreateContact struct {
+	FirstName   string `json:"first_name" binding:"required"`
+	LastName    string `json:"last_name" binding:"required"`
+	PhoneNumber string `json:"phone_number" binding:"required"`
+	Street      string `json:"street" binding:"required"`
+}
