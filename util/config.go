5d633d7c18cbdfb70ac968aece6521e3147106bc
hendisantika <hendisantika@yahoo.co.id>
2024-11-05 17:00:20 +0700

    Add LoadConfig in config.go🫸🌀✏️📗🐧🐳

--- util/config.go
+++ util/config.go
@@ -1,5 +1,7 @@
 package util
 
+import "github.com/spf13/viper"
+
 type Config struct {
 	DbDriver         string `mapstructure:"DB_DRIVER"`
 	DbSource         string `mapstructure:"DB_SOURCE"`
@@ -7,4 +9,20 @@ type Config struct {
 	PostgresPassword string `mapstructure:"POSTGRES_PASSWORD"`
 	PostgresDb       string `mapstructure:"POSTGRES_DB"`
 	ServerAddress    string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
