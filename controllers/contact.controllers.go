9ad6ff03908c63257dcfd94b8cb1d629b7582161
hendisantika <hendisantika@yahoo.co.id>
2024-11-05 17:02:11 +0700

    Add CreateContact in contact.controllers.go🫸🌀✏️📗🐧🐳

--- controllers/contact.controllers.go
+++ controllers/contact.controllers.go
@@ -2,6 +2,10 @@ package controllers
 
 import (
 	"context"
+	"github.com/gin-gonic/gin"
+	"golang-crud-rest/schemas"
+	"net/http"
+	"time"
 )
 
 type ContactController struct {
@@ -11,4 +15,33 @@ type ContactController struct {
 
 func NewContactController(db *db.Queries, ctx context.Context) *ContactController {
 	return &ContactController{db, ctx}
+}
+
+// Create contact  handler
+func (cc *ContactController) CreateContact(ctx *gin.Context) {
+	var payload *schemas.CreateContact
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "Failed payload", "error": err.Error()})
+		return
+	}
+
+	now := time.Now()
+	args := &db.CreateContactParams{
+		FirstName:   payload.FirstName,
+		LastName:    payload.LastName,
+		PhoneNumber: payload.PhoneNumber,
+		Street:      payload.Street,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+
+	contact, err := cc.db.CreateContact(ctx, *args)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "Failed retrieving contact", "error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "successfully created contact", "contact": contact})
 }
